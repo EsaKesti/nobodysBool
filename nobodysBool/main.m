bd4794abc4801e502c444fc03c2d169291807dbb
esa kesti <esakesti@esas-macbook-pro.local>
2017-02-28 21:49:05 +0000

    Initial Commit

---
+++ nobodysBool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nobodysBool
+//
+//  Created by esa kesti on 28/02/2017.
+//  Copyright © 2017 esa kesti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
